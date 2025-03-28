e5fe6603acff7b08902758aa08410bf940b4c6c9
PC-MAESTRO <PC-MAESTRO@DESKTOP-7N4A5CP>
2025-03-27 19:39:42 -0700

    Implementacion del diseño del componente home y la logica del componente home

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { subscribeOn } from 'rxjs';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-home',
-  standalone: false,
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public pokemons: any;
+  constructor(private router: Router, private ApiService: ApiService) { }
 
+  ngOnInit(): void {
+    this.getPokemonAll();
+  }
+
+  getPokemonAll() {
+    this.ApiService
+      .getPokemons()
+      .subscribe(resp => {
+        this.pokemons = resp;
+        // console.log(" ~ file: home.component.ts ~ line 21 ~ HomeComponent ~ getPokemonAll ~ this.pokemons", this.pokemons)
+      });
+  }
+  getMore(url: string) {
+    this.ApiService
+      .getByUrl(url)
+      .subscribe((resp: any) => {
+        this.pokemons.results = [... this.pokemons.results, ...resp.results]; //Partiendo de un array añade otro array al
+        this.pokemons.next = resp.next;
+      });
+  }
+
+  goToDetail(name: string){
+    this.router.navigate(['/details', name])
+  }
 }
